572512f0c4379bce598cdbbaaf45a9eb3d1b6a7c
tora180405 <toradigda@gmail.com>
2024-02-03 19:20:57 +0700

    add file

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,13 +1,22 @@
 <?php 
 
-class About{
+class About extends Controller{
 
-    public function index($nama = 'halo', $pekerjaan = 'halo'){
-        echo "Halo Nama saya $nama saya adalah $pekerjaan";
+    public function index($nama = 'halo', $pekerjaan = 'halo', $umur = 12){
+        $data['judul'] = 'About Me';
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
     public function page(){
-        echo 'About/page';
+        $data['judul'] = 'Page';
+        $this->view('templates/header', $data);
+        $this->view('About/page');
+        $this->view('templates/footer');
     }
 }
 
---
+++ app/views/about/index.php
@@ -0,0 +1,3 @@
+
+    <h1>About Me</h1>
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya dalah <?= $data['pekerjaan'] ?></p>
