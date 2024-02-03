e42b3462d9372e98b034f0c0c6c7baae2b213630
tora180405 <toradigda@gmail.com>
2024-02-03 20:33:06 +0700

    add file

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,2 +1,15 @@
-
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="<?= BASEURL ?>">PHP MVC</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="<?= BASEURL ?>">Home</a>
+                <a class="nav-link" href="<?= BASEURL ?>/about">About</a>
+            </div>
+        </div>
+    </div>
+</nav>
     <h1>Selamat Datang di Website Saya</h1>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -4,5 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman <?= $data['judul']; ?></title>
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
 </head>
 <body>
