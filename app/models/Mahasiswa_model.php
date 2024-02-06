cd3e852dacb3e2412392ef49e9507f8a861852f0
tora180405 <toradigda@gmail.com>
2024-02-06 19:12:41 +0700

    add file

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -27,26 +27,18 @@ class Mahasiswa_model{
     //     ]
     // ];
 
-    private $dbh; // database handler
-    private $stmt;
+
+    private $table = 'mahasiswa';
+    private $db;
 
     public function __construct()
     {
-        //data source name
-        $dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-        try{
-            $this->dbh = new PDO($dsn, 'root', '');
-        }catch(PDOException $e){
-            die($e->getMessage());
-        }
+        $this->db = new Database;
     }
 
     public function getAllMahasiswa(){
-        // return $this->mhs;
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->db->query('SELECT * FROM' . $this->table);
+        return $this->db->resultSet();
     }
 }
 ?>
