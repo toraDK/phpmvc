4f6ffca5f23da845da00552ad15fd9fef3f38119
tora180405 <toradigda@gmail.com>
2024-02-07 19:19:49 +0700

    edit file

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -3,16 +3,13 @@
     <div class="row">
         <div class="col-6">
             <h3>Daftar Mahasiswa</h3>
-            
-                <?php foreach($data['mhs'] as $mhs) :?>
-                    <ul>
-                        <li><?php echo $mhs['nama']; ?></li>
-                        <li><?php echo $mhs['nim']; ?></li>
-                        <li><?php echo $mhs['email']; ?></li>
-                        <li><?php echo $mhs['jurusan']; ?></li>
-                    </ul>
-                <?php endforeach; ?>
-
+            <?php foreach($data['mhs'] as $mhs) :?>
+            <ul class="list-group">
+                <li class="list-group-item d-flex justify-content-between align-items-start"><?php echo $mhs['nama']; ?>
+                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge text-bg-primary" style="text-decoration: none;">detail</a>
+                </li>
+            </ul>
+            <?php endforeach; ?>
         </div>
     </div>
 
